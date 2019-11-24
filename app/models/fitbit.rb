72fbe055a26560afd78e09dc4d96eccdf21e1a28
john barratt <MasterJohn@johns-MacBook-Pro.local>
2019-11-23 18:29:35 -0800

    refactor fitbit methods

--- app/models/fitbit.rb
+++ app/models/fitbit.rb
@@ -1,59 +1,59 @@
 require 'httparty'
+require 'base64'
 
 class Fitbit
   def self.get_tokens(code)
-    config = Rails.application.credentials.config
-    client_id = config[:client_id]
-    client_secret = config[:client_secret]
-    redirect_uri = config[:redirect_uri]
-    auth_code = Base64.encode64("#{client_id}:#{client_secret}")
-
     query = {
       'clientId' => client_id,
       'grant_type' => 'authorization_code',
       'redirect_uri' => redirect_uri,
       'code' => code
     }
-
-    headers = {
-      'Authorization' => "Basic #{auth_code}",
-      'Content-Type' => 'application/x-www-form-urlencoded'
-    }
-
-    res = HTTParty.post(
-      'https://api.fitbit.com/oauth2/token',
-      query: query,
-      headers: headers,
-      format: :plain
-    )
-
+    res = make_request(query)
     JSON.parse(res, symbolize_names: true)
   end
  
   def self.refresh_tokens
-    config = Rails.application.credentials.config
-    client_id = config[:client_id]
-    client_secret = config[:client_secret]
-    auth_code = Base64.encode64("#{client_id}:#{client_secret}")
-
     query = {
       'clientId' => client_id,
       'grant_type' => 'refresh_token',
       'refresh_token' => refresh_token
     }
+    res = make_request(query)
+    JSON.parse(res, symbolize_names: true)
+  end
 
-    headers = {
+  private
+
+  def self.client_id
+    Rails.application.credentials.config[:client_id]
+  end
+
+  def self.client_secret
+    Rails.application.credentials.config[:client_secret]
+  end
+
+  def self.auth_code
+    Base64.encode64("#{client_id}:#{client_secret}")
+  end
+
+  def self.redirect_uri
+    Rails.application.credentials.config[:redirect_uri]
+  end
+
+  def self.headers
+    {
       'Authorization' => "Basic #{auth_code}",
       'Content-Type' => 'application/x-www-form-urlencoded'
     }
+  end
 
-    res = HTTParty.post(
+  def self.make_request(query)
+    HTTParty.post(
       'https://api.fitbit.com/oauth2/token',
       query: query,
       headers: headers,
       format: :plain
     )
-
-    JSON.parse(res, symbolize_names: true)
   end
 end
