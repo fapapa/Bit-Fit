daa5b0e6f85fa6ddd5dc9b2ff85d6f86a5b2c717
john barratt <MasterJohn@johns-MacBook-Pro.local>
2019-11-25 12:02:03 -0800

    add axios before get calories controller from master

--- app/javascript/components/ProgressBar.js
+++ app/javascript/components/ProgressBar.js
@@ -1,10 +1,17 @@
-import React, { useState } from 'react'
-
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 export default function ProgressBar(props) {
-  const [percentage, setPercentage] = useState(60)
-
+  const [percentage, setPercentage] = useState(0)
+  
+  
+  
   const Filler = (props) => {
+    const user = {
+      calories_burned: 900,
+      daily_goal: 3000
+    }
+    const current_progress = setPercentage((user.calories_burned / user.daily_goal) * 100)
     return <div className="filler" style={{ width: `${percentage}%` }} />
   }
   return ( 
