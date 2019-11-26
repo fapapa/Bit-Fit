2ac85f6788108e48ab8e337bce03fd8dc94995b4
john barratt <MasterJohn@johns-MacBook-Pro.local>
2019-11-26 13:58:27 -0800

    progress bar takes calories from endpoints

--- app/javascript/components/ProgressBar.js
+++ app/javascript/components/ProgressBar.js
@@ -1,18 +1,9 @@
 import React from 'react';
 import Filler from './Filler';
-import classNames from 'classnames';
 
 export default function ProgressBar(props) {
-  console.log(props)
-  let progressBarClass = classNames('progress-bar', {
-    "progress-bar--calories": props.current_points,
-    "progress-bar--steps": props.current_points,
-    "progress-bar--health": props.current_health,
-    "progress-bar--experience": props.current_experience
-  });
-
   return ( 
-    <div className={progressBarClass}>
+    <div className="progress-bar">
       <Filler 
         current_points={props.current_points}
         daily_goal={props.daily_goal}
