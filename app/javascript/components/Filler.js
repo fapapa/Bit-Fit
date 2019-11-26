7a9f694e05dfe904583e245c2fb27352ab005b92
john barratt <MasterJohn@johns-MacBook-Pro.local>
2019-11-25 18:45:49 -0800

    style progress bar for homepage and make initial changes for progress bar component reusability

--- app/javascript/components/Filler.js
+++ app/javascript/components/Filler.js
@@ -1,17 +1,27 @@
-import React, { useState, useEffect} from 'react';
-
+import React, { useState, useEffect } from 'react';
+import classNames from 'classnames';
 export default function Filler(props) {
-  const [percentage, setPercentage] = useState(0)
+
+const [percentage, setPercentage] = useState(0)
+
+  const steps = props.steps;
+  const calories = props.calories;
+  
+  let fillerClass = classNames('filler', {
+    "filler--calories":props.calories,
+    "filler--steps": props.steps,
+    "filler--health": props.health,
+    "filler--experience": props.experience
+  });
 
   useEffect(() => {
+    setPercentage((calories / user.daily_goal) * 100)
+  }, [])
 
-    setPercentage((user.calories_burned / user.daily_goal) * 100)
-  })
   const user = {
-    calories_burned: 1800,
-    daily_goal: 3000
+    daily_goal: 3000 //props.goal
   }
 
 
-  return (<div className="filler" style={{ width: `${percentage}%` }} />)
+  return (<div className={fillerClass} style={{ width: `${percentage}%` }} />)
 }
--- app/javascript/components/ProgressBar.js
+++ app/javascript/components/ProgressBar.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import Filler from './Filler';
+import classNames from 'classnames';
 
 export default function ProgressBar(props) {
+
+  let progressBarClass = classNames('progress-bar', {
+    "progress-bar--calories": props.calories,
+    "progress-bar--steps": props.steps,
+    "progress-bar--health": props.health,
+    "progress-bar--experience": props.experience
+  });
+
   return ( 
-    <div className="progress-bar">
-      <Filler />
+    <div className={progressBarClass}>
+      <Filler 
+        calories={props.calories}
+        steps={props.steps}
+        health={props.health}
+        experience={props.experience}
+      />
     </div>
   )
 }
