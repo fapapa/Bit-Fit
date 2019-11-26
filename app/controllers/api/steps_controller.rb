f225270a02988346201ec9737de48727b7618b1d
Fabio Papa <fabtheman@gmail.com>
2019-11-26 11:17:32 -0800

    Add action to return steps taken for the day

--- app/controllers/api/steps_controller.rb
+++ app/controllers/api/steps_controller.rb
@@ -1,2 +1,26 @@
 class Api::StepsController < ApplicationController
+  before_action :require_authentication
+
+  def index
+    render json: current_user.steps_taken
+  end
+
+  private
+
+  def require_authentication
+    render(not_authenticated, status: 401) unless current_user
+  end
+
+  def current_user
+    return false unless session[:user_id]
+
+    @user ||= User.find(session[:user_id])
+  end
+
+  def not_authenticated
+    {json: {
+       error: 'Not authenticated',
+       status: 401
+     }}
+  end
 end
