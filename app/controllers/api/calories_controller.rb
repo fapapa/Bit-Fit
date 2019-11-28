72b85efb7fcf4e3b8349d30cfbe3427159fdd4c2
Fabio Papa <fabtheman@gmail.com>
2019-11-27 21:02:47 -0800

    Modify calories API to return active calories

--- app/controllers/api/calories_controller.rb
+++ app/controllers/api/calories_controller.rb
@@ -1,6 +1,6 @@
 class Api::CaloriesController < Api::ApiController
   def index
-    render json: current_user.calories_burned
+    render json: current_user.get_active_calories
   end
 
   def period
