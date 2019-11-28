8bd111f114c6aa99b317550403c0548bfcd6a738
Fabio Papa <fabtheman@gmail.com>
2019-11-27 17:40:29 -0800

    Fix missing key error

--- app/javascript/components/NavMenu.js
+++ app/javascript/components/NavMenu.js
@@ -1,21 +1,16 @@
-import React from "react"
+import React from "react";
 import AwesomeButton from "./AwesomeButton";
 
 export default function Application(props) {
-
-  const buttons = props.buttons.map(button => {
+  const buttons = props.buttons.map((button, index) => {
     return (
-      <AwesomeButton 
+      <AwesomeButton
+        key={index}
         title={button.title}
         onClick={button.onClick || null}
       />
-    )
+    );
   });
 
-
-  return (
-    <section className="navmenu">
-      {buttons}
-    </section>
-  );
+  return <section className="navmenu">{buttons}</section>;
 }
