dfac6ca2b28b1a36487e62711fcf7de77d9f4d6f
JakeFantin <jakefantin@gmail.com>
2019-11-28 10:54:55 -0800

    BattleSim: added the skeleton structure for the components for the battlesim"

---
+++ app/javascript/components/BattleSim/Versus.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Versus(props) {
+  return <div></div>;
+}
---
+++ app/javascript/components/BattleSim/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Loading from "./Loading";
+import Versus from "./Versus";
+import Battlefield from "./Battlefield";
+import useVisualMode from "hooks/useVisualMode";
+
+export default function Application(props) {
+  // DIFFERENT MODES FOR MODE SELECTOR
+  const LOADING = "LOADING";
+  const VERSUS = "VERSUS";
+  const BATTLEFIELD = "BATTLEFIELD";
+
+  // SELECTOR HOOK
+  const { mode, transition, back } = useVisualMode(
+    LOADING
+  );
+
+  return (<div className="battle-page">
+    {mode === LOADING && (
+      <Loading />
+    )}
+    {mode === VERSUS && (
+      <Versus />
+    )}
+    {mode === BATTLEFIELD && (
+      <Battlefield />
+    )}
+  </div>);
+}
