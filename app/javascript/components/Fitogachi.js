f892450a91ebcb7788c6c2f35495d26ea4ed997e
Fabio Papa <fabtheman@gmail.com>
2019-11-29 11:25:16 -0800

    Fetch fitogachi from API and display details

--- app/javascript/components/Fitogachi.js
+++ app/javascript/components/Fitogachi.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Axios from "axios";
 
 export default function Fitogachi(props) {
@@ -12,7 +12,22 @@ export default function Fitogachi(props) {
       });
   };
 
-  const [properties, setProperties] = useState(fetchFitogachi());
+  const [properties, setProperties] = useState({});
+  useEffect(() => {
+    fetchFitogachi();
+  }, []);
 
-  return <article className="Fitogachi">{}</article>;
+  return (
+    <article className="Fitogachi">
+      <h1>Fitgochi Properties</h1>
+      <ul>
+        <li>Name: {properties["name"]}</li>
+        <li>Color: {properties["color"]}</li>
+        <li>Level: {properties["level"]}</li>
+        <li>Current Exp: {properties["current_exp"]}</li>
+        <li>Current Energy: {properties["current_energy"]}</li>
+        <li>Last Experience: {properties["last_experience"]}</li>
+      </ul>
+    </article>
+  );
 }
