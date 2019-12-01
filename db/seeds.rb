72253037f866d407a7ba172334af01146fb031c9
Fabio Papa <fabtheman@gmail.com>
2019-11-30 17:14:39 -0800

    Create 1 years worth of random Day seeds for testing in dev

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+u = User.first
+((1.year.ago.to_date)..(Date.today.to_date)).to_a.each do |date|
+  u.days.create(
+    avg_heart_rate: Random.rand(50...80),
+    calories: Random.rand(2_000...2_500),
+    steps: Random.rand(2_000...16_000),
+    stats_date: date
+  )
+end
