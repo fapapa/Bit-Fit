249e0040528b63b944426b58cf0911d74e27e713
JakeFantin <jakefantin@gmail.com>
2019-12-01 15:58:17 -0800

    Icon: now have the power to support hue-rotation like fitogachi

--- app/javascript/components/Icon.js
+++ app/javascript/components/Icon.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Icon(props) {
-  return <img className="icon" src={`/images/icon-${props.color}-${props.user}.png`}></img>;
+  return <img 
+  className="icon" 
+  src={`/images/icon-${props.color}-${props.user}.png`}
+  style={{filter: `hue-rotate(${`${props.color}deg` || '0deg'})`}}
+  ></img>;
 }
